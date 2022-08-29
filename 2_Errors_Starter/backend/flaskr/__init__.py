4cde4e61b7a6f5a6fa02e0e3ec16b8ce59caefd2
Eric Clinton Appiah-Kubi <ecakubi@gmail.com>
2022-08-29 19:51:12 +0000

    Error handlers implemented.

--- 2_Errors_Starter/backend/flaskr/__init__.py
+++ 2_Errors_Starter/backend/flaskr/__init__.py
@@ -131,9 +131,42 @@ def create_app(test_config=None):
     #        Pay special attention to the status codes used in the aborts since those are relevant for this task!
 
     # @TODO: Write error handler decorators to handle AT LEAST status codes 400, 404, and 422.
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad request!"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Not found!"
+        })
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable!"
+        })
+
+    @app.errorhandler(405)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method not allowed!"
+        })
 
     # TEST: Practice writing curl requests. Write some requests that you know will error in expected ways.
     #       Make sure they are returning as expected. Do the same for other misformatted requests or requests missing data.
     #       If you find any error responses returning as HTML, write new error handlers for them.
+
+    # curl -X PUT -H "Content-Type: application/json" -d '{"author":"clinton"}' 127.0.0.1:5000/books
 
     return app
